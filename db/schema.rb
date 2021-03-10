72834bdac682ee9c2bedab984403221f68fadefb
jun kobayashi <junkobayashi1025@gmail.com>
2021-03-10 10:39:43 +0000

    migration修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_085026) do
+ActiveRecord::Schema.define(version: 2021_03_10_101101) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,11 +30,5 @@ ActiveRecord::Schema.define(version: 2021_03_10_085026) do
     t.string "password_digest"
   end
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  add_foreign_key "blogs", "users"
 end
